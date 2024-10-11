37a819680ea5f38bd2c652c51db3678520ad30e2
ThiagoShundi <shundi_hayashi@hotmail.com>
2024-10-11 13:58:04 -0300

    Projeto Finalizado

---
+++ Rankeada.js
@@ -0,0 +1,23 @@
+let vit = 50;
+let derr = 20;
+let niv = ""
+
+let cal = vit - derr;
+
+if (vit <= 10) {
+    niv = "Ferro"
+} else if (vit > 10 && vit <= 20) {
+    niv = "Bronze"
+} else if (vit > 10 && vit <= 20) {
+    niv = "Prata"
+} else if (vit > 20 && vit <= 50) {
+    niv = "Ouro"
+} else if (vit > 50 && vit <= 80) {
+    niv = "Diamante"
+} else if (vit > 80 && vit <= 90) {
+    niv = "Lendário"
+} else {
+    niv = "Imortal"
+}
+
+console.log(`O herói tem de saldo de ${cal} está no nível de ${niv}`)
